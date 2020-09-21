9a2f289acaf5d4308164606b2a7711b5f0c7f249
Luis Penaloza <ldp231@yahoo.com>
2020-09-21 20:28:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_value = nil 
-  lowesr_key = nil
+  lowest_key = nil
   name_hash.each do |key, value|
-    
+    if lowest_value > value
+      lowest_value = value
+      lowest_key = key 
+    end 
+  end
+  name_hash
 end
